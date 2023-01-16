27c45235e710e78b5cc43f8ce9dc488af0ce22de
bar spielberg <barspielberg@gmail.com>
2023-01-17 00:14:42 +0200

    🧹

--- src/sonarr/api.ts
+++ src/sonarr/api.ts
@@ -1,8 +1,23 @@
 const baseURL = 'http://10.0.0.55:8989/api/v3';
 
+const fetchTimeout = async (
+    url: string,
+    init: RequestInit & { timeout: number }
+) => {
+    const controller = new AbortController();
+    const id = setTimeout(() => {
+        controller.abort();
+    }, init.timeout);
+    try {
+        return await fetch(url, { ...init, signal: controller.signal });
+    } finally {
+        clearTimeout(id);
+    }
+};
+
 export async function health() {
     try {
-        await fetch(baseURL + '/health');
+        await fetchTimeout(baseURL + '/health', { timeout: 5 * 1000 });
         return true;
     } catch (error) {
         return false;
