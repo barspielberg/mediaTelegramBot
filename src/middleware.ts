0384c0a72a70500482418b67825401ce6d12a471
bar spielberg <barspielberg@gmail.com>
2023-02-03 15:32:46 +0200

    log chatId

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,10 @@ import { Middleware } from './pkg/grammy.ts';
 
 const allowedChats: number[] = JSON.parse(config.ALLOWED_CHATS);
 
-export const log: Middleware = async ({ message, callbackQuery }, next) => {
+export const log: Middleware = async ({ message, callbackQuery, chat }, next) => {
     const text = message?.text;
     const data = callbackQuery?.data;
-    console.log(JSON.stringify({ text, data }));
+    console.log(chat?.id ?? '-', JSON.stringify({ text, data }));
     await next();
 };
 
